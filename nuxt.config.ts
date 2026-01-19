cdaad83cb08265d0690eebcfe6ec828aa296a150
Niels Rol <niels@example.com>
2026-01-19 15:40:49 +0100

    Deploy workflow en nuxt fonts + Icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+    css: [
+    '@/assets/css/fonts.css',
+    '@/assets/css/variables.css',
+    '@/assets/css/main.css'
+  ],
+
+  modules: ['@nuxt/fonts', '@nuxt/icon'],
+  ssr: false,
+
+  app: {
+    baseURL: '/nielsrol.nl/',
+    buildAssetsDir: '_nuxt/'
+  }
 })
